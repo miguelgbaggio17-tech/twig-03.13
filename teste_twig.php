68a3c913d55d7b62630e4937fb4682a95b726c81
Miguel Baggio <miguelgbaggio17@gmail.com>
2026-03-16 16:55:40 -0300

    Adição de um poema

--- teste_twig.php
+++ teste_twig.php
@@ -9,10 +9,18 @@ $displinas = [
     'Interface Web',
     'Desenvolvimento de Sistemas',
 ];
-
+$poema = 'Se tu me amas, ama-me baixinho
+    Não o grites de cima dos telhados
+    Deixa em paz os passarinhos
+    Deixa em paz a mim!
+    Se me queres,
+    enfim,
+    tem de ser bem devagarinho, Amada,
+    que a vida é breve, e o amor mais breve ainda...';
 
 echo $twig->render('teste_twig.html', [
     'nome' => $nome,
     'legal' => true,
     'disciplinas' => $displinas,
+    'poema' => $poema,
 ]);
